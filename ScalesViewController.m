c4b8c0f7c511cd7cc14053a289e339c2b1c4c89c
Elena Boyd <elenaeboyd@gmail.com>
2015-02-03 10:45:41 -0600

    more

--- ScalesViewController.m
+++ ScalesViewController.m
@@ -58,9 +58,7 @@
 
     
     self.view.backgroundColor                            = [UIColor GuitarCream];
-    self.navigationController.navigationBar.barTintColor = [UIColor GuitarBlue];
-    self.navigationController.navigationBar.opaque       = YES;
-//    self.navigationController.navigationBar.translucent       = NO;
+    self.navigationController.navigationBar.barTintColor = [UIColor GuitarBlue];    
 
     self.navigationController.navigationBar.tintColor    = [UIColor whiteColor];
     [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName,[UIFont blackoutFontWithSize:22.0f], NSFontAttributeName, nil]];
